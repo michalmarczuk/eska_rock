0ead4739d8b424d5f0d0f7903ae27523f3b5dfbd
Michał Marczuk <michal@marczuk.org>
2019-06-11 15:27:37 +0200

    Small improvement on getting master window object in PlayStopButton class.

--- play_stop_button.py
+++ play_stop_button.py
@@ -13,7 +13,6 @@ class PlayStopButton(tkinter.Button):
         self.play_image = PhotoImage(file="images/play.png")
         self.stop_image = PhotoImage(file="images/stop.png")
         self.player = Player()
-        self.parent = parent
 
         super().__init__(
             parent,
@@ -24,7 +23,7 @@ class PlayStopButton(tkinter.Button):
 
     def play_stop_icon_change(self, image: PhotoImage):
         self.configure(command=partial(self.play_stop_icon_change, self["image"]), image=image)
-        self.parent.update()
+        self.master.update()
 
         if self.play():
             self.player.stop()
